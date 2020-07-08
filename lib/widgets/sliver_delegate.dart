fbeff1685c434cf5535ca3ed59d58ebeb0c3e30f
jeffreyconcha <depanconcha@gmail.com>
2020-07-08 11:35:23 +0800

    b

---
+++ lib/widgets/sliver_delegate.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SliverTabBarDelegate extends SliverPersistentHeaderDelegate {
+  final double height, elevation;
+  final Color background;
+  final TabBar tabBar;
+
+  SliverTabBarDelegate({
+    @required this.tabBar,
+    this.height,
+    this.background = Colors.white,
+    this.elevation,
+  });
+
+  @override
+  Widget build(BuildContext context, double offset, bool overlap) {
+    return Material(
+      color: background,
+      elevation: elevation,
+      child: tabBar,
+    );
+  }
+
+  @override
+  double get maxExtent => height ?? tabBar.preferredSize.height;
+
+  @override
+  double get minExtent => height ?? tabBar.preferredSize.height;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
+}
