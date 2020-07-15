151881644c5843f016a82ee8243ca21477ef119a
jeffreyconcha <depanconcha@gmail.com>
2020-07-15 17:12:13 +0800

    b

---
+++ lib/widgets/icon.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class PanIcon extends StatelessWidget {
+  final EdgeInsetsGeometry margin, padding;
+  final Alignment alignment;
+  final Color color, background;
+  final double width, height;
+  final String icon;
+  final BoxFit fit;
+
+  const PanIcon({
+    Key key,
+    @required this.icon,
+    this.width,
+    this.height,
+    this.fit = BoxFit.contain,
+    this.alignment = Alignment.center,
+    this.color,
+    this.background,
+    this.margin,
+    this.padding,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      margin: margin,
+      padding: padding,
+      color: background,
+      child: SvgPicture.asset(
+        'assets/icons/$icon.svg',
+        color: color,
+        fit: fit,
+        alignment: alignment,
+      ),
+    );
+  }
+}
