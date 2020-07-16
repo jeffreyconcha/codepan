406823a0b0d103f971c9aa234c3f8c6202403d26
jeffreyconcha <depanconcha@gmail.com>
2020-07-16 19:53:56 +0800

    b

--- lib/widgets/text.dart
+++ lib/widgets/text.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:codepan/resources/colors.dart';
 
+typedef OnTextOverflow = void Function(int lines);
+
 class PanText extends StatelessWidget {
   final double width, height, fontSize, fontHeight, radius, borderWidth;
   final Color fontColor, background, borderColor;
   final EdgeInsetsGeometry margin, padding;
+  final OnTextOverflow onTextOverflow;
+  final TextDirection textDirection;
   final TextDecoration decoration;
   final String text, fontFamily;
   final FontWeight fontWeight;
@@ -28,6 +32,7 @@ class PanText extends StatelessWidget {
     this.alignment = Alignment.center,
     this.background = PanColors.none,
     this.textAlign = TextAlign.center,
+    this.textDirection = TextDirection.ltr,
     this.overflow,
     this.decoration,
     this.radius = 0,
@@ -39,32 +44,65 @@ class PanText extends StatelessWidget {
     this.height,
     this.maxLines,
     this.shadows,
+    this.onTextOverflow,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final style = TextStyle(
+      color: fontColor,
+      fontFamily: fontFamily,
+      fontStyle: fontStyle,
+      fontWeight: fontWeight,
+      height: fontHeight,
+      fontSize: fontSize,
+      decoration: decoration,
+      shadows: shadows,
+    );
+    final child = onTextOverflow != null && maxLines != null
+        ? LayoutBuilder(
+            builder: (ctx, c) {
+              final span = TextSpan(
+                text: text ?? '',
+                style: style,
+              );
+              final painter = TextPainter(
+                maxLines: maxLines,
+                textAlign: textAlign,
+                textDirection: textDirection,
+                text: span,
+              );
+              painter.layout(maxWidth: c.maxWidth);
+              if (painter.didExceedMaxLines) {
+                if (onTextOverflow != null) {
+                  final lines = painter.computeLineMetrics();
+                  onTextOverflow.call(lines.length);
+                }
+              }
+              return Text.rich(
+                span,
+                overflow: overflow,
+                maxLines: maxLines,
+                textAlign: textAlign,
+                textDirection: textDirection,
+              );
+            },
+          )
+        : Text(
+            text ?? '',
+            style: style,
+            overflow: overflow,
+            maxLines: maxLines,
+            textAlign: textAlign,
+            textDirection: textDirection,
+          );
     return Container(
       width: width,
       height: height,
       margin: margin,
       padding: padding,
       alignment: alignment,
-      child: Text(
-        text ?? '',
-        overflow: overflow,
-        maxLines: maxLines,
-        style: TextStyle(
-          color: fontColor,
-          fontFamily: fontFamily,
-          fontStyle: fontStyle,
-          fontWeight: fontWeight,
-          height: fontHeight,
-          fontSize: fontSize,
-          decoration: decoration,
-          shadows: shadows,
-        ),
-        textAlign: textAlign,
-      ),
+      child: child,
       decoration: BoxDecoration(
         color: background,
         borderRadius: BorderRadius.circular(radius),
