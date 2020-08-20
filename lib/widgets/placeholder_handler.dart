4c3b6aa5b66d766401e97848f99646448d0fe945
jeffreyconcha <depanconcha@gmail.com>
2020-08-20 14:53:46 +0800

    b

--- lib/widgets/placeholder_handler.dart
+++ lib/widgets/placeholder_handler.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class PlaceholderHandler extends StatelessWidget {
   final Widget child;
   final Widget placeholder;
+  final EdgeInsets margin;
+  final EdgeInsets padding;
+  final double width;
+  final double height;
   final bool condition;
 
   const PlaceholderHandler({
@@ -10,11 +14,19 @@ class PlaceholderHandler extends StatelessWidget {
     @required this.child,
     this.placeholder,
     this.condition = false,
+    this.margin,
+    this.padding,
+    this.width,
+    this.height,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: width,
+      height: height,
+      margin: margin,
+      padding: padding,
       child: condition ? child : placeholder,
     );
   }
