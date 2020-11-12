fef537e229062ac7a7d12ee4678c16ba09d539bb
jeffreyconcha <depanconcha@gmail.com>
2020-11-12 16:08:00 +0800

    b'

--- lib/extensions/map_ext.dart
+++ lib/extensions/map_ext.dart
@@ -1,18 +1,36 @@
 import 'package:codepan/utils/codepan_utils.dart';
 
+const prefixKey = '\$prefix';
+
 extension MapUtils on Map<String, dynamic> {
   int getInt(String key) {
-    final value = this[key];
+    final value = this[getKey(key)];
     return PanUtils.parseInt(value);
   }
 
   double getDouble(String key) {
-    final value = this[key];
+    final value = this[getKey(key)];
     return PanUtils.parseDouble(value);
   }
 
   bool getBool(String key) {
-    final value = this[key];
+    final value = this[getKey(key)];
     return PanUtils.parseBool(value);
+  }
+
+  dynamic get(String key) {
+    return this[getKey(key)];
+  }
+
+  void setPrefix(String prefix) {
+    this[prefixKey] = prefix;
+  }
+
+  String getKey(String key) {
+    final prefix = this[prefixKey];
+    if (prefix != null) {
+      return '$prefix.$key';
+    }
+    return key;
   }
 }
