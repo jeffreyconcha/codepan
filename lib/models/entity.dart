5948aac6718abda6d110308d6bda5c69a47e7894
jeffreyconcha <depanconcha@gmail.com>
2020-11-12 20:16:01 +0800

    b'

--- lib/models/entity.dart
+++ lib/models/entity.dart
@@ -3,13 +3,11 @@ import 'package:codepan/database/schema.dart';
 abstract class EntityData {
   const EntityData();
 
-  dynamic get entity;
+  TableSchema get schemaInstance;
 
-  DatabaseSchema get schemaInstance;
+  String get table => schemaInstance.tableName;
 
-  String get table => schemaInstance.tableName(entity);
+  String get unique => schemaInstance.unique;
 
-  String get unique => schemaInstance.unique(entity);
-
-  List<String> get uniqueGroup => schemaInstance.uniqueGroup(entity);
+  List<String> get uniqueGroup => schemaInstance.uniqueGroup;
 }
