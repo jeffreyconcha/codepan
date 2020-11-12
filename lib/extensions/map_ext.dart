6e9a1a906e69825e2f424aae8d176138a83b851d
jeffreyconcha <depanconcha@gmail.com>
2020-11-12 15:59:40 +0800

    b

--- lib/extensions/map_ext.dart
+++ lib/extensions/map_ext.dart
@@ -1,17 +1,17 @@
 import 'package:codepan/utils/codepan_utils.dart';
 
 extension MapUtils on Map<String, dynamic> {
-  int toInt(String key) {
+  int getInt(String key) {
     final value = this[key];
     return PanUtils.parseInt(value);
   }
 
-  double toDouble(String key) {
+  double getDouble(String key) {
     final value = this[key];
     return PanUtils.parseDouble(value);
   }
 
-  bool toBool(String key) {
+  bool getBool(String key) {
     final value = this[key];
     return PanUtils.parseBool(value);
   }
