60824a3e59a2967ba29997a8b7f771b0fc5426c1
jeffreyconcha <depanconcha@gmail.com>
2020-11-13 22:12:27 +0800

    b

---
+++ lib/bloc/parent_event.dart
@@ -0,0 +1,15 @@
+import 'package:codepan/bloc/parent_state.dart';
+import 'package:equatable/equatable.dart';
+
+class ParentEvent extends Equatable {
+  final ParentState state;
+
+  const ParentEvent(this.state);
+
+  bool isMirroredFrom<State extends ParentState>() {
+    return state is State;
+  }
+
+  @override
+  List<Object> get props => [state];
+}
---
+++ lib/bloc/parent_state.dart
@@ -0,0 +1,8 @@
+import 'package:codepan/bloc/parent_event.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class ParentState<Event extends ParentEvent> extends Equatable {
+  const ParentState();
+
+  Event mirrorToEvent();
+}
