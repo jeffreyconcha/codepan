6582c1e9ad3da4326af4418adf7409372b71e360
jeffreyconcha <depanconcha@gmail.com>
2021-01-14 16:56:20 +0800

    'b'

--- lib/utils/permission_handler.dart
+++ lib/utils/permission_handler.dart
@@ -17,7 +17,7 @@ abstract class StateWithPermission<T extends StatefulWidget>
 
   void onPermissionResult(bool isGranted);
 
-  /// Return [bool] value of true when prompting using a dialog <br/>
+  /// Return [bool] value of true when prompting a dialog to the user.<br/>
   /// [onDialogDetach] - Callback function to notify the permission
   /// handler that the dialog has been detached.
   bool onPermanentlyDenied(
