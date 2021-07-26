50250ce4545aecd87ace417b1099560a7639c2fa
jeffreyconcha <depanconcha@gmail.com>
2021-07-26 14:51:18 +0800

    b

---
+++ lib/utils/search_handler.dart
@@ -0,0 +1,41 @@
+import 'package:codepan/models/master.dart';
+import 'package:codepan/utils/debouncer.dart';
+import 'package:flutter/cupertino.dart';
+
+abstract class StateWithSearch<S extends StatefulWidget, M extends MasterData>
+    extends State<S> {
+  final Debouncer _debouncer = Debouncer();
+  final List<M> _suggestions = [];
+  String _search = '';
+
+  List<M> get allItems;
+
+  List<M> get items => showSuggestions ? _suggestions : allItems;
+
+  bool get showSuggestions => _search.isNotEmpty;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void onSearch(text) {
+    _debouncer.run(() {
+      _search = text.toLowerCase();
+      _suggestions.clear();
+      if (_search.isNotEmpty) {
+        for (final item in allItems) {
+          if (item.name!.toLowerCase().contains(_search)) {
+            _suggestions.add(item);
+          }
+        }
+      }
+      setState(() {});
+    });
+  }
+}
