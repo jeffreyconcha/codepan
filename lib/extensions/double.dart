da3b597ac03166aa6f5e3e00ee35ef6aca6a7cc8
jeffreyconcha <depanconcha@gmail.com>
2021-07-28 18:05:44 +0800

    b

--- lib/extensions/double.dart
+++ lib/extensions/double.dart
@@ -6,8 +6,16 @@ extension DoubleUtils on double {
     return nf.format(this);
   }
 
-  String format(String format) {
-    final nf = NumberFormat(format, 'en_US');
-    return nf.format(this);
+  String format({String? format}) {
+    if (format != null) {
+      final nf = NumberFormat(format, 'en_US');
+      return nf.format(this);
+    } else {
+      if (this % 1 == 0) {
+        return this.toStringAsFixed(0);
+      } else {
+        return this.toMoneyFormat();
+      }
+    }
   }
 }
