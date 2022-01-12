c45556b2c3b1b91b559ce0dee77a133baa8e192c
jeffreyconcha <depanconcha@gmail.com>
2022-01-12 18:58:44 +0800

    'b'

---
+++ lib/widgets/header_body.dart
@@ -0,0 +1,58 @@
+import 'package:codepan/resources/dimensions.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HeaderBody extends StatelessWidget {
+  final Color? headerColor, backgroundColor, shadowColor;
+  final double? headerHeight, shadowBlurRadius;
+  final Offset? shadowOffset;
+  final Widget header;
+  final Widget? body;
+
+  const HeaderBody({
+    Key? key,
+    required this.header,
+    this.body,
+    this.headerHeight,
+    this.headerColor = Colors.white,
+    this.backgroundColor,
+    this.shadowColor,
+    this.shadowBlurRadius,
+    this.shadowOffset,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final d = Dimension.of(context);
+    final t = Theme.of(context);
+    final height = headerHeight ?? d.at(60);
+    return Stack(
+      children: [
+        Container(
+          margin: EdgeInsets.only(
+            top: height,
+          ),
+          color: backgroundColor,
+          child: body,
+        ),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            height: headerHeight ?? d.at(60),
+            decoration: BoxDecoration(
+              color: headerColor,
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: shadowColor ?? t.shadowColor,
+                  blurRadius: shadowBlurRadius ?? d.at(3),
+                  offset: shadowOffset ?? Offset(0, d.at(2)),
+                )
+              ],
+            ),
+            child: header,
+          ),
+        ),
+      ],
+    );
+  }
+}
