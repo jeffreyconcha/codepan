1e961310b6dbfccf0057a2b5e29ef67056e9b18c
jeffreyconcha <depanconcha@gmail.com>
2022-10-20 16:51:38 +0800

    'b'

--- lib/utils/debouncer.dart
+++ lib/utils/debouncer.dart
@@ -3,24 +3,30 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 
 class Debouncer {
+  final ValueChanged<bool>? statusNotifier;
   final int milliseconds;
+  bool _isActive = false;
   Timer? _timer;
 
-  Debouncer({this.milliseconds = 500});
+  Debouncer({
+    this.milliseconds = 500,
+    this.statusNotifier,
+  });
 
   void run(VoidCallback action) {
-    if (_timer != null) {
-      _timer!.cancel();
-    }
-    _timer = Timer(
-      Duration(milliseconds: milliseconds),
-      action,
-    );
+    cancel();
+    _timer = Timer(Duration(milliseconds: milliseconds), () {
+      action.call();
+      statusNotifier?.call(false);
+      _isActive = false;
+    });
   }
 
   void cancel() {
-    if (_timer != null) {
-      _timer!.cancel();
+    _timer?.cancel();
+    if (!_isActive) {
+      statusNotifier?.call(true);
+      _isActive = true;
     }
   }
 }
