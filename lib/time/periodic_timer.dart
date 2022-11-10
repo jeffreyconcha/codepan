91f59221281dedd2ac5631dae2aa6619168ace46
jeffreyconcha <depanconcha@gmail.com>
2022-11-10 10:50:42 +0800

    b

--- lib/time/periodic_timer.dart
+++ lib/time/periodic_timer.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 
+import 'package:codepan/time/time.dart';
 import 'package:flutter/cupertino.dart';
 
 class PeriodicTimer {
   final ValueChanged<Timer>? runner;
   final Duration interval;
+  late final Time _time;
   Timer? _timer;
   int _tick = 0;
 
@@ -12,11 +14,14 @@ class PeriodicTimer {
 
   bool get isActive => _timer?.isActive ?? false;
 
+  Time get time => _time;
+
   PeriodicTimer({
     required this.interval,
     this.runner,
     bool autoStart = true,
   }) {
+    _time = Time.now();
     if (autoStart) {
       _initialize();
     }
