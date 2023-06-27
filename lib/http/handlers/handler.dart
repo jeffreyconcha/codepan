be83e62a4ced4a02a0d7f163a61ad02b7967e53e
jeffreyconcha <depanconcha@gmail.com>
2023-06-28 00:55:57 +0800

    b

--- lib/http/handlers/handler.dart
+++ lib/http/handlers/handler.dart
@@ -1,9 +1,7 @@
 abstract class InitHandler {
   final bool allowEmpty;
 
-  const InitHandler([
-    this.allowEmpty = true,
-  ]);
+  const InitHandler(this.allowEmpty);
 
   List<Map<String, dynamic>> init(
     Map<String, dynamic> body,
