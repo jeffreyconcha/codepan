90c7c032a5385a3af67ac0222135b8c65fa14b02
jeffreyconcha <depanconcha@gmail.com>
2024-05-24 15:04:28 +0800

    b

--- lib/extensions/num.dart
+++ lib/extensions/num.dart
@@ -1,5 +1,9 @@
+import 'dart:math' as m;
+
 extension NumExt on num {
   bool isBetween(num start, num end) {
-    return this >= start && this <= end;
+    final min = m.min(start, end);
+    final max = m.max(start, end);
+    return this >= min && this <= max;
   }
 }
